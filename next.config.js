33347987237fcb63867ee41a06e61444d76debc6
Cem <65294379+cemtatli@users.noreply.github.com>
2024-04-09 12:04:08 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['', ''], // Add your domains here
-    deviceSizes: [320, 375, 768, 1024, 1280, 1920],
+    deviceSizes: [375, 640, 768, 1024, 1280, 1440, 1920],
   },
 }
 
