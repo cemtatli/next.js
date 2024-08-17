b7b787693eb656ab467139feaacec72dd0f25e78
Cem Tatlı <cemtatli3535@gmail.com>
2024-08-17 22:50:11 +0300

    Update dependencies and configuration files

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter as FontSans } from 'next/font/google'
 import localFont from 'next/font/local'
 
 import './globals.css'
-import { cn } from '@/lib/utils'
+import { cn } from '../lib/utils'
 
 import { sharedTitle, sharedDescription } from './shared-metadata'
 
